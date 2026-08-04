9613193e9caed64dcb65300adffea0eaa132a19b
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-08-04 12:21:46 +0200

    added code to HTML, CSS and JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector(".subscribe-form");
 const input = document.querySelector(".email-input");
+const errorMessage = document.querySelector(".error-message");
 
 function validateInput(email){
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -10,5 +11,23 @@ function validateInput(email){
 form.addEventListener("submit", event => {
     event.preventDefault();
 
+    const email = input.value.trim();
+
+    if(email === ""){
+        input.classList.add("error");
+        errorMessage.textContent = "Whoops! It looks like you forgot to add your email";
+        errorMessage.classList.add("show");
+        return;
+    }
     
+    if(!validateInput(email)){
+        input.classList.add("error");
+        errorMessage.textContent = "Please provide a valid email";
+        errorMessage.classList.add("show");
+        return;
+    }
+
+    input.classList.remove("error");
+    errorMessage.textContent = "";
+    errorMessage.classList.remove("show");
 });
